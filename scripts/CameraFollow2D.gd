26014cb942bc5d5e9edd81dc0f39bab174e96d75
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-09-20 19:54:28 -0400

    learned how the $ works

--- scripts/CameraFollow2D.gd
+++ scripts/CameraFollow2D.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
-export(NodePath) var player_path: NodePath
-onready var player = get_node(player_path)
+onready var player = $"../Player"
 
 func _physics_process(delta):
 	position += (player.get_position()-position)*0.2
